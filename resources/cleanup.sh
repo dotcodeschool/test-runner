eb52e21c68871e3de53cf86bc317de8cf6360087
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-05-13 15:13:22 +0530

    move userfs out of jail path during cleanup to persist changes

--- resources/cleanup.sh
+++ resources/cleanup.sh
@@ -5,6 +5,7 @@
 function cleanup_all {
     echo "Performing full cleanup for VM ID: $1"
     pgrep -f "firecracker --id $1" | xargs -r sudo kill
+    ./manage_fs.sh --move-out "$1"
     ./manage_fs.sh --cleanup
     ./manage_network.sh --cleanup "$1"
     ./manage_users.sh --delete "$1"
