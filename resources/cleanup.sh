975f6a9f6d9db0314431819bd907624eeb4829f5
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-05-13 20:00:29 +0530

    - update function name from cleanup_all to cleanup - add command to remove instance's jailer directory - enforce command pattern to use a VM ID

--- resources/cleanup.sh
+++ resources/cleanup.sh
@@ -2,14 +2,24 @@
 
 # Usage: cleanup.sh --vm-id <VM ID>
 
-function cleanup_all {
+function cleanup {
     echo "Performing full cleanup for VM ID: $1"
     pgrep -f "firecracker --id $1" | xargs -r sudo kill
     ./manage_fs.sh --move-out "$1"
     ./manage_fs.sh --cleanup
+    rm -r "/srv/jailer/firecracker/$1" || true
     ./manage_network.sh --cleanup "$1"
     ./manage_users.sh --delete "$1"
     # Additional cleanup as necessary
 }
 
-cleanup_all "$2"
+case "$1" in
+    --vm-id)
+        cleanup "$2"
+       ;;
+    *)
+        echo "Invalid option: $1"
+        echo "Usage: $0 --vm-id <VM ID>"
+        exit 1
+        ;;
+esac
